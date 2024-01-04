77f1c078cc771833391b342c19367276e6faaee4
Antotray <ahmadsantoso800@gmail.com>
2024-01-04 23:52:35 +0700

    First commit

---
+++ Java OOP/src/programmer/zaman/now/application/StackTraceApp.java
@@ -0,0 +1,33 @@
+package programmer.zaman.now.application;
+
+public class StackTraceApp {
+    public static void main(String[] args) {
+//        try {
+//            String[] names = {
+//                    "Ahmad", "Santoso", "Tray"
+//            };
+//            System.out.println(names[100]);
+//        } catch (Throwable throwable) {
+//            StackTraceElement[] stackTraceElements = throwable.getStackTrace();
+//
+//            throwable.printStackTrace();
+//        }
+
+        try {
+            sampleError();
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void sampleError() {
+        try {
+            String[] names = {
+                    "Ahmad", "Santoso", "Tray"
+            };
+            System.out.println(names[100]);
+        } catch (Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
+}

b6d75d94e5e81b884204ca0328fccb32260d20d4
Antotray <ahmadsantoso800@gmail.com>
2024-01-05 23:28:32 +0700

    first commit

--- Java OOP/src/programmer/zaman/now/application/CarApp.java
+++ Java OOP/src/programmer/zaman/now/application/CarApp.java
@@ -1,7 +1,9 @@
 package programmer.zaman.now.application;
+import programmer.zaman.now.annotation.Fancy;
 import programmer.zaman.now.data.Avanza;
 import programmer.zaman.now.data.Car;
 
+@Fancy(name = "AnimalApp", tage = {"application", "java"})      // Ini Annotation
 public class CarApp {
     public static void main(String[] args) {
         Car car = new Avanza();
